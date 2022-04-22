6f51492203c0425189f9f1e2a2fa0d4ea9581aaa
Vinson Martin <vm84039@yahoo.com>
2022-04-21 18:39:25 -0700

    make it better

--- resources/views/bookListing.blade.php
+++ resources/views/bookListing.blade.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        @include("includes.head")
-        @section('title', 'Dashboard')
-    </head>
-    <body >
-        <header style= "margin-left:200px">
-           @include("customer.customerHeader")
-        </header>
-        <main >
-                @include("includes.bookTable")
-        </main>
-    @include("includes.footer")
-    @include("includes.scripts")
-    </body>
-</html>
+@extends('layouts.appMaster')
+@section('title', 'Dashboard')
+@include("includes.bookTable")
+@include("includes.footer")
+@include("includes.scripts")
