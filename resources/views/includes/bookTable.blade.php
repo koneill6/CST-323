094a51dee1b6395efaaccdb00624a6e77e73afe2
Vinson Martin <vm84039@yahoo.com>
2022-04-21 18:43:25 -0700

    Milestone2

--- resources/views/includes/bookTable.blade.php
+++ resources/views/includes/bookTable.blade.php
@@ -18,7 +18,7 @@ $books = $DAO->getAllBooks();
     foreach($books as $book){
     ?>
     <tr class="bookRow">
-        <td class="rowImg" rowspan="4"><img  class="tableImg" src="{{ asset('img')}}/{{ $book->getIMG() }}"></td>
+        <td class="rowImg" rowspan="4"><img  alt="pic" class="tableImg" src="{{ asset('img')}}/{{ $book->getIMG() }}"></td>
         <td class="table-title" colspan="4"><?php echo $book->getTitle();?></td>
         <td rowspan="2">
             <a href="#" class="navbar-brand">
@@ -29,7 +29,6 @@ $books = $DAO->getAllBooks();
                      stroke="#087f5b"
                      color="blue"
                      width="px"
-                     paddding="3px"
                      stroke-width=".5">
                     <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>                                    </svg>
             </a>
@@ -41,11 +40,11 @@ $books = $DAO->getAllBooks();
         <?php if($book->available = 1) {?>
         <td>Available</td> <?php }
         else {?>
-        <td><?php echo $book->getDueDate();?></td>  <?php } ?>
+        <td><?php echo $book->getDueDate();?></td>  <?php } ?></tr>
     <tr>
         <th class="table-heading">Publisher</th>
         <td><?php echo $book->getPublisher();?></td>
-        <th class="table-heading">Genre</td>
+        <th class="table-heading">Genre</th>
         <td style="width: 3%;"><?php echo $book->getGenre();?></td>
         <td rowspan="2">
             <a href="#" class="navbar-brand">
@@ -56,7 +55,6 @@ $books = $DAO->getAllBooks();
                      stroke="#087f5b"
                      color="black"
                      width="10px"
-                     paddding="3px"
                      stroke-width=".5">
                     <path d="m9.708 6.075-3.024.379-.108.502.595.108c.387.093.464.232.38.619l-.975 4.577c-.255 1.183.14 1.74 1.067 1.74.72 0 1.554-.332 1.933-.789l.116-.549c-.263.232-.65.325-.905.325-.363 0-.494-.255-.402-.704l1.323-6.208Zm.091-2.755a1.32 1.32 0 1 1-2.64 0 1.32 1.32 0 0 1 2.64 0Z"/></svg></a>
         </td>
